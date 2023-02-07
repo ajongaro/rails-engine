71509c985b818a35ab2b57eaaf29808b8647f930
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-02-06 20:35:56 -0700

    Add merchant show endpoint

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -23,4 +23,20 @@ RSpec.describe "Merchant API" do
       end
     end
   end
+
+  describe 'the merchant show endpoint' do
+    it 'sends a single merchant' do
+      merchant1 = Merchant.first
+      get "/api/v1/merchants/#{merchant1.id}"
+
+      expect(response).to be_successful 
+      
+      merchant = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(merchant).to have_key(:id)
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:id]).to be_an(String)
+      expect(merchant[:attributes][:name]).to be_a(String)
+    end
+  end
 end
