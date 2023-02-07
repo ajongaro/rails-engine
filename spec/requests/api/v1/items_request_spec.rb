1aaed94864612c4663a057cfaf1134dbaf1a41a5
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-02-06 22:22:55 -0700

    Add tests for item show and item index endpoints

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 RSpec.describe "Items API" do
   let!(:merchant1) { create(:merchant) }
   let!(:merchant2) { create(:merchant) }
-   
-  before :each do
-    create_list(:item, 5, merchant:  merchant1 )
-  end
+  let!(:item1) { create(:item, merchant: merchant1) }
+  let!(:item2) { create(:item, merchant: merchant1) }
 
   describe 'the items index endpoint' do
     it 'sends a list of all items' do
-      get '/api/v1/items/'
+      create_list(:item, 5, merchant:  merchant1 )
+
+      get api_v1_items_path 
 
       expect(response).to be_successful
 
       items = JSON.parse(response.body, symbolize_names: true)[:data]
 
-      expect(items.count).to eq(5)
+      expect(items.count).to eq(7)
 
       items.each do |item|
         expect(item).to have_key(:id)
@@ -30,6 +30,27 @@ RSpec.describe "Items API" do
         expect(item[:attributes]).to have_key(:merchant_id)
         expect(item[:attributes][:merchant_id]).to be_a(Integer)
       end
+    end
+  end
+
+  describe 'the item show endpoint' do
+    it 'sends the details of one item' do
+      get api_v1_item_path(item1)
+
+      expect(response).to be_successful
+
+      item = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(String)
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
     end
   end
 end
