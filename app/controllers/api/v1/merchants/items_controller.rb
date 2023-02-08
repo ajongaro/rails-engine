6d6f2b7a3d33acaca9ec1e0c74fbd48c042d5a04
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-02-07 20:57:12 -0700

    Add edge case for merchant items

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::Merchants::ItemsController < ApplicationController
   def index
-    items = Merchant.find(params[:merchant_id]).items
-    render json: ItemSerializer.new(items)
+    merchant = Merchant.find(params[:merchant_id])
+
+    if merchant
+      render json: ItemSerializer.new(merchant.items)
+    else
+      render json: { error: "Merchant not found" }, status: :not_found
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: 'merchants/items'
       end
-
       resources :items, only: [:index, :create, :update, :destroy, :show] do
         resources :merchant, only: [:index], controller: 'items/merchants'
       end
